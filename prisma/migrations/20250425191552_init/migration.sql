613c938bf379fc298322e76e3a8a151e61fcaf9b
OliverioJunior <oliverio.junior2@gmail.com>
2025-04-25 16:51:00 -0300

    feat(database): add Prisma setup and initial schema for product management
    This commit introduces Prisma as the ORM for the database, including the initial schema for product and category management. It also adds the necessary Prisma client configuration and updates the package.json with relevant scripts and dependencies to support database operations.

---
+++ prisma/migrations/20250425191552_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "produtos" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "preco" DOUBLE PRECISION NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "categoriaId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "produtos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categorias" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categorias_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categorias_nome_key" ON "categorias"("nome");
+
+-- AddForeignKey
+ALTER TABLE "produtos" ADD CONSTRAINT "produtos_categoriaId_fkey" FOREIGN KEY ("categoriaId") REFERENCES "categorias"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
